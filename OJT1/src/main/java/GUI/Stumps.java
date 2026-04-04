ff383507d380704fd6da184bcdc83c07d73d8ea3
TES <taaviaro@student.uef.fi>
2026-04-04 19:33:28 +0300

    SQL yhteyden luonti ja env muuttujien käyttö lisätty. nyt vaan kirjoittaa operaatiot siihen päälle.

--- OJT1/src/main/java/GUI/Stumps.java
+++ OJT1/src/main/java/GUI/Stumps.java
@@ -3,7 +3,9 @@ package GUI;
 import Structs.Kayttaja;
 
 public class Stumps {
-    static Kayttaja getKayttaja(String ID){
+		//SQLbridge bridge;
+
+    public Kayttaja getKayttaja(String ID){
         //TODO hakee kayttajan databasesta.
         return new Kayttaja("Erkki Pertti", "12345", "test@test.com");
     }
