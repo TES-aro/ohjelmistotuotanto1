f92a9158f5f10d4b51d629c596cbcf4439c57439
TES <taaviaro@student.uef.fi>
2026-04-04 16:12:59 +0300

    cleaned up repo, started SQLbackend

---
+++ OJT1/src/main/java/SQLbackend/SQLbridge.java
@@ -0,0 +1,11 @@
+package SQLbackend;
+
+public class SQLbridge {
+    //TODO ottaa yhteyden SQL palvelimeen.
+    //write ja fetch operaatiot.
+    //käyttäkää Struct paketin olioita public
+    //metodejen palautuksissa ja inputeissa.
+
+    //jos ja kun jotain puuttuu niistä niin lisätkää
+    //ja mainitkaa disccordissa
+}
