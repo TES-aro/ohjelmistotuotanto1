792395c2c95661d3b1207d26305668d859affb31
ii-hyv <iihyv@proton.me>
2026-05-20 00:41:41 +0300

    Update Mokki.java

--- OJT1/src/main/java/Structs/Mokki.java
+++ OJT1/src/main/java/Structs/Mokki.java
@@ -2,48 +2,41 @@ package Structs;
 
 public class Mokki {
     int ID;
-    //String omistajaID;
-    // voidaan myös toteuttaa luokallisesti vaatimalla Omistaja ryhmän olio.
-    // Omistaja omistaja;
-    //String osoite;
+    String omistajaID;
+    String osoite;
     int maksimiAsukkaat;
     boolean onkoVarattu;
     double hinta;
-    // mahdollisesti muut tiedot kuten koko, huoneet jne.
 
-    public Mokki(int ID, int maksimiAsukkaat, boolean onkoVarattu, double hinta){
+    public Mokki(int ID, String omistajaID, String osoite, int maksimiAsukkaat, boolean onkoVarattu, double hinta){
         this.ID = ID;
-        //this.omistajaID = omistajaID;
-        //this.osoite = osoite;
+        this.omistajaID = omistajaID;
+        this.osoite = osoite;
         this.maksimiAsukkaat = maksimiAsukkaat;
         this.onkoVarattu = onkoVarattu;
         this.hinta = hinta;
     }
-    public Mokki(int ID, int maksimiAsukkaat, double hinta){
-        this.ID = ID;
-        this.maksimiAsukkaat = maksimiAsukkaat;
-        this.hinta = hinta;
-    }
-
 
     // getterit
     public int getID() {
         return ID;
     }
-    //public String getOmistajaID() {
-    //    return omistajaID;
-    //}
-
-    //public String getOsoite() {
-    //    return osoite;
-    //}
+    public String getOmistajaID() {
+        return omistajaID;
+    }
+    public String getOsoite() {
+       return osoite;
+    }
 
     public int getMaksimiAsukkaat() {return maksimiAsukkaat;}
     public boolean getOnkoVarattu() {return onkoVarattu;}
     public double getHinta() {return hinta;}
 
     // setterit
-    //public void setOsoite(String osoite) { this.osoite = osoite; }
+    public void setID(int ID) { this.ID = ID; }
+    public void setOmistajaID(String omistajaID) { this.omistajaID = omistajaID; }
+    public void setOsoite(String osoite) { this.osoite = osoite; }
     public void setMaksimiAsukkaat(int maksimiAsukkaat) { this.maksimiAsukkaat = maksimiAsukkaat; }
     public void setOnkoVarattu(boolean onkoVarattu) {this.onkoVarattu = onkoVarattu;}
+    public void setHinta(double hinta) {this.hinta = hinta;}
 }
