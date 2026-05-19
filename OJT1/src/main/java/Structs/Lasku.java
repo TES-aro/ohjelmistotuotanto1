a84ba7bcced79420dc05fbb6b8aa774d5baf232b
ii-hyv <iihyv@proton.me>
2026-05-20 00:35:13 +0300

    Update Lasku.java

--- OJT1/src/main/java/Structs/Lasku.java
+++ OJT1/src/main/java/Structs/Lasku.java
@@ -3,21 +3,32 @@ package Structs;
 import java.util.Date;
 
 public class Lasku {
-    Kayttaja maksaja;
-    Varaus varaus;
-    Laskutettava[] laskutettavat;
-    boolean maksettu; // lisätty
-    Date erapaiva; // lisätty
+    private int laskuID;
+    private Kayttaja maksaja;
+    private Varaus varaus;
+    private Laskutettava[] laskutettavat;
+    private boolean maksettu;
+    private Date luontipvm;
+    private Date erapaiva;
 
-    public Lasku(Kayttaja maksaja, Varaus varaus, boolean maksettu, Date erapaiva, Laskutettava[] laskutettavat){
+    public Lasku(Kayttaja maksaja, Varaus varaus, boolean maksettu, Date luontipvm, Date erapaiva, Laskutettava[] laskutettavat){
         this.maksaja = maksaja;
         this.varaus = varaus;
         this.maksettu = maksettu;
+        this.luontipvm = luontipvm;
         this.erapaiva = erapaiva;
         this.laskutettavat = laskutettavat;
     }
 
     // getterit
+
+    public int getLaskuID() {
+        return laskuID;
+    }
+
+    public Date getLuontipvm() {
+        return luontipvm;
+    }
 
     public Kayttaja getMaksaja() {
         return maksaja;
@@ -39,9 +50,12 @@ public class Lasku {
         return erapaiva;
     }
 
+
     // setterit
 
     public void setMaksettu (boolean maksettu) {this.maksettu = maksettu;}
+    public void setLaskuID(int laskuID) { this.laskuID = laskuID; }
+    public void setLuontipvm(Date luontipvm) { this.luontipvm = luontipvm; }
     public void setErapaiva(Date erapaiva)    { this.erapaiva = erapaiva; }
 
 }
